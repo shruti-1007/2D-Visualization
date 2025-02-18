54fc36149fcbb573b127a4b649dbc15b8aaf31c1
Shruti Maharjan <080bct082.shruti@pcampus.edu.np>
2025-02-18 07:10:48 +0545

    Add initial files

---
+++ src/ddaAlgorithm.cpp
@@ -0,0 +1,21 @@
+#include <graphics.h>
+#include <iostream>
+#include <cmath>
+
+void drawDdaLine(int x1, int y1, int x2, int y2, int color) {
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+    float xInc = dx / float(steps);
+    float yInc = dy / float(steps);
+    float x = x1;
+    float y = y1;
+
+    setcolor(color); // Set the color for the line
+
+    for (int i = 0; i <= steps; i++) {
+        putpixel(int(x), int(y), color);
+        x += xInc;
+        y += yInc;
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <graphics.h>
+#include <iostream>
+#include "bargraph.cpp"
+
+#include "pie_chart.cpp"
+// #include "line_graph.cpp"
+// #include "transformations.cpp"
+// #include "export.cpp"
+
+using namespace std;
+
+void displayMenu() {
+    cout << "Data Visualization Dashboard" << endl;
+    cout << "1. Bar Chart" << endl;
+    cout << "2. Pie Chart" << endl;
+    cout << "3. Line Graph" << endl;
+    cout << "4. Exit" << endl;
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)"");
+    setbkcolor(WHITE);
+    cleardevice();
+    int choice;
+    do {
+        displayMenu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+               bargraph();
+                break;
+            case 2:
+             
+                break;
+            case 3:
+               // Calls the function to draw line graph
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice, try again." << endl;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
